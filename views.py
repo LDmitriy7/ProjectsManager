252cee2cde31020a65c4f4bf23023c85df2e12cd
levch <levchenko.d.a1998@gmail.com>
2022-06-04 00:06:04 +0300

    .

--- views.py
+++ views.py
@@ -1,25 +1,34 @@
 import os
-from pathlib import Path
-
-from flask import request
 
 import api
-from loader import app
 import config
+from loader import app
 
 
-@app.route('/edit/<path:file_path>', methods=['GET', 'POST'])
-def edit(file_path: str):
-    file_path = config.PROJECTS_DIR / file_path
+@app.route('/get/<path:path>')
+def get(path: str):
+    path = config.PROJECTS_DIR / path
 
-    if request.method == 'GET':
-        with open(file_path, encoding='utf-8') as file:
-            return {'text': file.read()}
-    elif text := request.json['text']:
-        with open(file_path, encoding='utf-8', mode='w') as file:
-            file.write(text.replace('\r\n', '\n'))
+    if not os.path.exists(path):
+        return {'ok': False, 'error': 'Not found'}
 
-        return {'ok': True}
+    if os.path.isdir(path):
+        walker = os.walk(path)
+        items = next(walker)
+        return {'ok': True, 'type': 'dir', 'files': items[1] + items[2]}
+
+    try:
+        with open(path, encoding='utf-8') as file:
+            return {'ok': True, 'type': 'file', 'text': file.read()}
+    except Exception as e:
+        return {'ok': False, 'error': e}
+
+
+# elif text := request.json['text']:
+#     with open(file_path, encoding='utf-8', mode='w') as file:
+#         file.write(text.replace('\r\n', '\n'))
+#
+#     return {'ok': True}
 
 
 @app.route('/deploy/<project>')
